5b74d371a287efc842d34028acd8f13151951a4f
mancle@naver.com <mancle@naver.com>
2018-09-03 23:32:05 +0900

    vi update

---
+++ vi_monitor.py
@@ -0,0 +1,48 @@
+import time
+import requests
+import datetime
+import json
+
+OTP_URL = "http://marketdata.krx.co.kr/contents/COM/GenerateOTP.jspx?bld=MKD%2F10%2F1002%2F10020408%2Fmkd10020408&name=form&_" + str(int(time.time()*1000))
+AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.186 Safari/537.36"
+HEADER = {'user-agent':AGENT, 'X-Requested-With': 'XMLHttpRequest'}
+DATE = datetime.date.today()
+TODAY = DATE.strftime("%Y%m%d")
+picked = []
+
+SEARCH_URL = "http://marketdata.krx.co.kr/contents/MKD/99/MKD99000001.jspx"
+ID = "chat_id=476315430&"
+SEND_URL = "https://api.telegram.org/bot641542576:AAHNabxUsCq5nqRmADV2ebNt_NrjjpVl9pg/sendMessage?"
+
+OTP = requests.get(OTP_URL,headers=HEADER)
+OTP = OTP.text
+print(TODAY)
+
+REQUEST = {
+    'mkt_tp_cd': 'ALL',
+    'vi_kind_cd': 'ALL',
+    'isu_cdnm': '전체',
+    'isu_cd': '',
+    'isu_nm': '',
+    'isu_srt_cd': '',
+    'fr_work_dt': TODAY,
+    'to_work_dt': TODAY,
+    'pagePath': '/contents/MKD/10/1002/10020408/MKD10020408.jsp',
+    'code': OTP,
+    'pageFirstCall': 'Y'
+}
+
+
+value = requests.post(SEARCH_URL, REQUEST, HEADER)
+#print(value.text)
+value = json.loads(value.text)
+
+
+for item in value['result']:
+    vi_activated = item['isu_abbrv']
+    print(vi_activated)
+    if vi_activated not in picked :
+        requests.get(SEND_URL + ID + "text=vi발동!!!!" + vi_activated)
+        picked.append(vi_activated)
+
+time.sleep(1)
---
+++ vi_monitoring.py
@@ -0,0 +1,50 @@
+from bs4 import BeautifulSoup
+import requests
+import datetime
+import time
+import pandas as pd
+
+URL = 'http://www.etoday.co.kr/news/hotissue/newsman_txt.php?eid=minori3032'
+send_URL = 'https://api.telegram.org/bot503225439:AAFVv3WnsASUlJ-SHbBjobaO9dArzN9pCbk/sendMessage?'
+ID = "chat_id=-235881804&"
+AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.186 Safari/537.36"
+HEADER = {'user-agent':AGENT}
+
+def get_list():
+
+    df = pd.DataFrame(columns=['time', 'title', 'contents'])
+
+    html = requests.get(URL, headers=HEADER).text
+    soup = BeautifulSoup(html, 'html.parser')
+    area = soup.find("ul", {"class": "news_lst news_lst2"})
+    list = area.find_all("li")
+
+    for element in list :
+        title = element.find("a", {"class": "tit"}).text
+        contents = element.find("p").find("a").text
+        time = element.find("span").text
+        time = time.replace(" ","")
+        time = datetime.datetime.strptime(time,"%Y-%m-%d%H:%M")
+        df.loc[df.shape[0]] = [time, title, contents]
+
+    return df
+
+NOW = datetime.datetime.now()
+LAST = NOW
+END = NOW + datetime.timedelta(hours = 8)
+
+while NOW < END :
+    df = get_list()
+    NOW = datetime.datetime.now()
+
+    for i in range(df.shape[0]) :
+
+        if df.loc[i, 'time'] > LAST :
+            if df.loc[i, 'title'].find('[특징주]') > -1 :
+                print(df.loc[i, 'title'])
+                TEXT = 'text=' + df.loc[i, 'title']
+                requests.get(send_URL+ID+TEXT)
+                TEXT = 'text=' + df.loc[i, 'contents']
+                requests.get(send_URL+ID+TEXT)
+            LAST = NOW
+    time.sleep(3)
